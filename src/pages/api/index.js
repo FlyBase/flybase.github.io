01fb1503e809503cc03165a9a3e230d5f28b2e4d
Josh Goodman <jogoodma@indiana.edu>
2018-09-20 16:05:47 -0400

    Quick and dirty addition of swagger-ui.

---
+++ src/pages/api/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import SwaggerUI from 'swagger-ui'
+
+import 'swagger-ui/dist/swagger-ui.css'
+
+import Layout from '../../components/layout'
+
+import { spec } from './openapi';
+
+class Api extends Component {
+
+  componentDidMount() {
+    SwaggerUI({
+      dom_id: '#swagger-container',
+      spec: spec
+    })
+  }
+
+  render() {
+
+    return (
+      <Layout>
+        <div id={'swagger-container'} />
+      </Layout>
+    )
+  }
+
+
+}
+
+export default Api
