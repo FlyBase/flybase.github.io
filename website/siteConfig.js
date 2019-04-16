b70f17e86be729829dc7b1f3cfa19953bf056967
Josh Goodman <jogoodma@iu.edu>
2019-04-16 11:24:26 -0400

    Publishing issues

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'flybase.github.io',
+  projectName: 'FlyBase.github.io',
   organizationName: 'FlyBase',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
